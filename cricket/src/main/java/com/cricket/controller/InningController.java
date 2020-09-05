d68197c63e83f33b7f6508287d6a2822dffc8d60
Suhas <suhascomp@gmail.com>
2020-09-05 13:36:18 +0530

    Added Inning API's

--- cricket/src/main/java/com/cricket/controller/InningController.java
+++ cricket/src/main/java/com/cricket/controller/InningController.java
@@ -3,10 +3,9 @@ package com.cricket.controller;
 import com.cricket.model.Inning;
 import com.cricket.service.InningService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/inning")
@@ -15,8 +14,28 @@ public class InningController {
     @Autowired
     InningService inningService;
 
-    @PostMapping()
+    @PostMapping("/save")
     public void saveInning(@RequestBody Inning inning) {
         inningService.saveInning(inning);
+    }
+
+    @GetMapping("/all-innings")
+    public List<Inning> getAllInnings() {
+        return inningService.getAllInnings();
+    }
+
+    @PutMapping("/update")
+    public void updateInning(@RequestBody Inning inning) {
+        inningService.updateInning(inning);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteInningById(@PathVariable int id) {
+        inningService.deleteInningById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Inning getInningById(@PathVariable int id) {
+        return inningService.getInningById(id);
     }
 }
--- cricket/src/main/java/com/cricket/service/InningService.java
+++ cricket/src/main/java/com/cricket/service/InningService.java
@@ -5,6 +5,8 @@ import com.cricket.repo.InningRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InningService {
     @Autowired
@@ -12,5 +14,21 @@ public class InningService {
 
     public void saveInning(Inning inning) {
         inningRepo.save(inning);
+    }
+
+    public List<Inning> getAllInnings() {
+        return inningRepo.findAll();
+    }
+
+    public void updateInning(Inning inning) {
+        inningRepo.save(inning);
+    }
+
+    public void deleteInningById(int id) {
+        inningRepo.deleteById(id);
+    }
+
+    public Inning getInningById(int id) {
+        return inningRepo.findById(id).get();
     }
 }
