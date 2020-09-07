c253b99d06adce6633eed178cbd57824c87e3d79
Suhas <suhascomp@gmail.com>
2020-09-07 23:37:48 +0530

    Added Address class and added base url.

---
+++ cricket/src/main/java/com/cricket/model/Address.java
@@ -0,0 +1,63 @@
+package com.cricket.model;
+
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "default_gen", sequenceName = "address_seq", allocationSize = 1)
+public class Address extends BaseEntity {
+    private String addressLine1;
+    private String addressLine2;
+    private String state;
+    private String country;
+    private String city;
+    private long postalCode;
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public long getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(long postalCode) {
+        this.postalCode = postalCode;
+    }
+}
