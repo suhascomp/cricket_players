53c7c63af2964cd40a7cfb253ea2e56e8f821c41
Suhas <suhascomp@gmail.com>
2020-09-07 23:35:45 +0530

    added layout components

--- frontend/CricketPlayers/src/app/layout/layout.module.ts
+++ frontend/CricketPlayers/src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { BlankComponent } from './blank/blank.component';
+import { CommonLayoutComponent } from './common-layout/common-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BlankComponent, CommonLayoutComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
