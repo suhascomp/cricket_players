b8d39de4db5a52b218517540d4d87bd54578f4d4
Suhas <suhascomp@gmail.com>
2020-09-08 23:19:59 +0530

    added header footer menu components.

--- frontend/CricketPlayers/src/app/layout/layout.module.ts
+++ frontend/CricketPlayers/src/app/layout/layout.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { BlankComponent } from './blank/blank.component';
 import { CommonLayoutComponent } from './common-layout/common-layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
-  declarations: [BlankComponent, CommonLayoutComponent],
+  declarations: [BlankComponent, CommonLayoutComponent, FooterComponent, HeaderComponent, MenuComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
